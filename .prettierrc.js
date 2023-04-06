93bd2b552d65906b9094f5ba7b9aa78bfef36e99
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2023-04-06 09:07:15 +0700

    first init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: false,
+  semicolons: true,
+  trailingComma: "all",
+};
